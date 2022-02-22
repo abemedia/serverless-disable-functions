70dbfb4c04dd3cdbc9d4975b7d1e207803a3c89c
Richard Goldstein <richgoldmd@gmail.com>
2022-02-22 13:46:18 -0500

    Added schema validation for each standard provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 'use strict';
 
+
+const providers = [
+  'aws', 'azure', 'aliyun', 'cloudflare', 'fn', 'google', 'knative', 'kubeless', 'openwhisk', 'spotinst', 'tencent'
+];
+
 class ServerlessDisableFunctionPlugin {
   constructor(serverless) {
     this.serverless = serverless;
+
+    // Add a validation for each of the standard providers
+    providers.forEach(provider => {
+
+      serverless.configSchemaHandler.defineFunctionProperties(provider, {
+        properties: {
+          enabled: { type: 'boolean' },
+        },
+      });
+    });
     this.hooks = { 'before:package:initialize': this.run.bind(this) };
   }
 
-  run() {
+  run () {
     Object.entries(this.serverless.service.functions).forEach(([key, func]) => {
       if (func.enabled !== undefined && !func.enabled) {
         this.serverless.cli.log('Disabling function: ' + key);
