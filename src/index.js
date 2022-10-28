0d39f1bc4db3d53c384adf1c0c6b212b924413c5
Zeph / Liz Loss-Cutler-Hull <warp@aehallh.com>
2022-10-28 01:22:51 -0700

    feat: add support for disabling events
    This allows us to remove events from functions in the same way that we
    can currently remove functions, with the very notable change that the
    keyword that we look for is 'disabled' instead of 'enabled'.
    
    We do this because in the AWS provider, the 'enabled' keyword already
    exists for some event types, with an existing meaning.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,27 @@ class ServerlessDisableFunctionPlugin {
         enabled: { type: 'boolean' },
       },
     });
+
+    // Add validation for events.  We use disabled here because enabled has existing meanings.
+    Object.entries(
+      serverless.configSchemaHandler.schema.properties.functions.patternProperties
+    ).forEach(([, obj]) => {
+      Object.entries(obj.properties.events.items.anyOf).forEach(([, item]) => {
+        if (item.required == '__schemaWorkaround__') {
+          // Must be skipped, or we fail.
+          return;
+        }
+        serverless.configSchemaHandler.defineFunctionEventProperties(
+          serverless.service.provider.name,
+          item.required[0],
+          {
+            properties: {
+              disabled: { type: `boolean` },
+            },
+          }
+        );
+      });
+    });
     this.hooks = { 'before:package:initialize': this.run.bind(this) };
   }
 
@@ -18,6 +39,15 @@ class ServerlessDisableFunctionPlugin {
       if (func.enabled !== undefined && !func.enabled) {
         this.serverless.cli.log('Disabling function: ' + key);
         delete this.serverless.service.functions[key];
+      } else {
+        Object.entries(func.events).forEach(([i, event]) => {
+          Object.entries(event).forEach(([ekey, e]) => {
+            if (e.disabled !== undefined && e.disabled) {
+              this.serverless.cli.log(`Disabling function '${key}' event '${ekey}'.`);
+              delete this.serverless.service.functions[key].events[i][ekey];
+            }
+          });
+        });
       }
     });
   }
